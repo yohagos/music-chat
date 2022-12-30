a4497ef01624a0cc2a585a9bddb0e48cf8d053c4
yosefha <yosef.hagos@sva.de>
2022-12-30 15:45:15 +0100

    get request list

--- controller/contacts.py
+++ controller/contacts.py
@@ -1,9 +1,15 @@
 from fastapi import status, HTTPException
 from sqlalchemy.orm import Session
 
+from schemas.models import ContactRequests as ContactRequestModel
 from utitlities.logged_in import get_user
 from utitlities.util import getTimeStamp
 
-def accept_contact_request():
+def accept_contact_request(db: Session):
     pass
 
+def get_request_list(db: Session):
+    return db.query(ContactRequestModel).all()
+
+def decline_contact_request(id: int, db: Session):
+    pass
--- routes/contacts.py
+++ routes/contacts.py
@@ -1,7 +1,12 @@
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
+from typing import List
 
+from authentication.oauth2 import get_current_user
+from schemas.users import UserBase
+from schemas.contacts import ShowContactRequest, ContactRequestBase
 from database.db import get_db
+from controller.contacts import *
 
 router = APIRouter(
     prefix='/contacts',
@@ -9,9 +14,14 @@ router = APIRouter(
 )
 
 @router.post('/accepts')
-def accept_contact():
+def accept_contact(request: ContactRequestBase, db: Session = Depends(get_db), current_user: UserBase = Depends(get_current_user)):
     pass
 
+@router.get('/req_list', response_model=List[ShowContactRequest])
+def request_list(db: Session = Depends(get_db), current_user: UserBase = Depends(get_current_user)):
+    return get_request_list(db)
+
 @router.delete('/{id}')
-def decline_request(id: int):
+def decline_request(id: int, db: Session = Depends(get_db), current_user: UserBase = Depends(get_current_user)):
     pass
+
