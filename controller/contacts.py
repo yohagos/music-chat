d7541b9886948f37c6ffb09de8d8110b6513e002
yosefha <yosef.hagos@sva.de>
2022-12-30 16:17:49 +0100

    create request improved

--- controller/contacts.py
+++ controller/contacts.py
@@ -1,7 +1,7 @@
 from fastapi import status, HTTPException
 from sqlalchemy.orm import Session
 
-from schemas.models import ContactRequests as ContactRequestModel
+from schemas.models import ContactRequests as ContactRequestModel, User as UserModel
 from utitlities.logged_in import get_user
 from utitlities.util import getTimeStamp
 
@@ -10,10 +10,24 @@ def accept_contact_request(request: ContactRequestModel, db: Session):
 
 def create_new_requests(request: ContactRequestModel, db: Session):
     user = get_user()
-    print(request)
+    sender = db.query(UserModel).filter(UserModel.username == user).first()
+    if request.user is request.requested:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail=f'User {request.user} cannot request Contact to themself')
+    elif sender is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'User {user} not found')
+    # how to create sqlalchemy query with join?
+# q = Session.query(
+#          User, Document, DocumentPermissions,
+#     ).filter(
+#          User.email == Document.author,
+#     ).filter(
+#          Document.name == DocumentPermissions.document,
+#     ).filter(
+#         User.email == 'someemail',
+#     ).all()
 
     # Check request infos!
-
+    print(request)
     db.add(request)
     db.commit()
     db.refresh(request)
