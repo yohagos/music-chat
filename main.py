a0b2dccf2284e67a13ebf5be3780460897fc5acd
yosefha <yosef.hagos@sva.de>
2023-01-06 16:25:40 +0100

    testing for frontend routes

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from routes import users, music, login, messages, contacts
 from database.db import engine
@@ -9,6 +10,16 @@ from utitlities.util import checkMusicFolder
 checkMusicFolder()
 
 app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(login.router)
 app.include_router(users.router)
